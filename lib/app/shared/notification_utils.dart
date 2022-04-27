fc363289535c6efe4fd09126f8d1743e05476699
angelo-squadra <angelo.savioti@squadra.com.br>
2022-04-27 11:12:48 -0300

    added local notifications package to the project

--- lib/app/shared/notification_utils.dart
+++ lib/app/shared/notification_utils.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationsUtils {
@@ -11,4 +14,91 @@ class NotificationsUtils {
 
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
+
+  static const AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails(
+    '1', //Required for Android 8.0 or after
+    'default', //Required for Android 8.0 or after
+    channelDescription: 'default_channel', //Required for Android 8.0 or after
+    importance: Importance.defaultImportance,
+    priority: Priority.defaultPriority,
+  );
+
+  static const IOSNotificationDetails iOSPlatformChannelSpecifics =
+      IOSNotificationDetails(
+    presentAlert:
+        null, // Present an alert when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+    presentBadge:
+        null, // Present the badge number when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+    presentSound:
+        true, // Play a sound when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+    //sound: String?,  // Specifics the file path to play (only from iOS 10 onwards)
+    //badgeNumber: int?, // The application's icon badge number
+    //attachments: List<IOSNotificationAttachment>?, (only from iOS 10 onwards)
+    //subtitle: String?, //Secondary description  (only from iOS 10 onwards)
+    //threadIdentifier: String? /*(only from iOS 10 onwards)*/,
+  );
+
+  Future<void> showNotification() async {
+    NotificationDetails? platformSpecificDetails;
+
+    if (Platform.isIOS) {
+      platformSpecificDetails = const NotificationDetails(
+        iOS: iOSPlatformChannelSpecifics,
+      );
+    }
+
+    if (Platform.isAndroid) {
+      platformSpecificDetails = const NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+      );
+    }
+
+    if (platformSpecificDetails != null) {
+      await flutterLocalNotificationsPlugin.show(
+        1,
+        "A Notification From My Application",
+        "This notification was sent using Flutter Local Notifcations Package",
+        platformSpecificDetails,
+        payload: 'data',
+      );
+    }
+  }
+
+  Future<void> initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    const initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    const initializationSettingsIos = IOSInitializationSettings(
+      requestSoundPermission: false,
+      requestBadgePermission: false,
+      requestAlertPermission: false,
+    );
+
+    const initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIos);
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: selectNotification,
+    );
+  }
+
+  Future<void> requestIosPermissions() async {
+    if (Platform.isIOS) {
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              IOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+            alert: true,
+            badge: true,
+            sound: true,
+          );
+    }
+  }
+
+  Future selectNotification(String? payload) async {
+    // Handle notification tapped logic here
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:study_flutter_local_push_notifications/app/app.dart';
+import 'package:study_flutter_local_push_notifications/app/shared/notification_utils.dart';
 
-void main() {
+void main() async {
+  await NotificationsUtils.instance.initialize();
+  await NotificationsUtils.instance.requestIosPermissions();
   runApp(const App());
 }
